cf0cfe075c8c2d4c8ba5d5e2b16ce897ff3308d7
Nargiza Supabaeva <supabaevanargiza@gmail.com>
2024-12-22 23:23:40 +0600

    Initial commit: добавлены task1 и task2

---
+++ task1/Main.java
@@ -0,0 +1,16 @@
+package kg.attractor.java.task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var cats = Cat.makeCats(10);
+        Printer.print(cats);
+
+        // А сюда добавьте код, который будет сортировать коллекцию котов
+        // используйте лямбда-выражения и ссылки на методы
+        // cats.sort(?);
+        // Printer.print(cats);
+    }
+
+}
